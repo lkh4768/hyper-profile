3d6544cab53cf57ac6b5ce297d8b03a540b7d87b
lkh4768 <lkh5510@gmail.com>
2019-08-10 15:51:06 +0900

    feat(menu): add profile menu on plugins

--- src/index.js
+++ src/index.js
@@ -1,1 +1,15 @@
-exports.onApp = () => {};
+exports.decorateMenu = menu =>
+  menu.map(item => {
+    if (item.label !== 'Plugins') return item;
+
+    const newItem = {...item};
+
+    newItem.submenu = newItem.submenu.concat({
+      label: 'Profile',
+      accelerator: 'Alt+C',
+      click: () => {
+        console.log('Clicked profile menu');
+      },
+    });
+    return newItem;
+  });
